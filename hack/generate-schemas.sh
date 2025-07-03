56828198eb9e95ce86367407773b00ab6f165e3b
Tobias Schlicht <tobias.schlicht@sap.com>
2025-07-03 14:04:31 +0200

    Remove prefix from APIResourceSchemas

--- hack/generate-schemas.sh
+++ hack/generate-schemas.sh
@@ -9,29 +9,13 @@ TMP_DIR=${REPO_ROOT}/tmp
 EXPORT_FILE=${REPO_ROOT}/schemas/gardener/apiexport-controlplane.cluster.x-k8s.io.yaml
 
 echo "⚙️ Generating APIResouceSchemas for gardener"
+mkdir -p ${TMP_DIR}
 apigen --input-dir ${REPO_ROOT}/config/crd/bases --output-dir ${TMP_DIR}
 
 new_schema_files=$(find ${TMP_DIR} -type f | grep -E 'apiresourceschema.*\.yaml$')
 
-old_schema_names=$(cat ${EXPORT_FILE} | yq .spec.latestResourceSchemas | grep -E 'controlplane|infrastructure' | sed 's/^- //' )
-new_schema_names=()
-
 for schema_file in ${new_schema_files}; do
-  new_schema_names+=($(yq .metadata.name ${schema_file}))
+  yq eval '.metadata.name |= sub("^v[0-9]+-[a-f0-9]+\.", "")' "${schema_file}" -i -P
 done
-
 echo "🔄 Replacing old APIResourceSchemas"
 cp ${new_schema_files} ${REPO_ROOT}/schemas/gardener
-
-echo "📝 Updating APIExport"
-# Remove old schemas from list
-for old_schema in ${old_schema_names}; do
-  yq -i "del(.spec.latestResourceSchemas[] | select(. == \"${old_schema}\"))" ${EXPORT_FILE}
-done
-
-# Prepend new schemas to list
-for new_schema in "${new_schema_names[@]}"; do
-  if ! yq '.spec.latestResourceSchemas[]' "${EXPORT_FILE}" | grep -qx "${new_schema}"; then
-    yq -i ".spec.latestResourceSchemas = [\"${new_schema}\"] + .spec.latestResourceSchemas" "${EXPORT_FILE}"
-  fi
-done
