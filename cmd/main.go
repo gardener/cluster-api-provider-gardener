024455660add40ebe8d266c306ee8c5f1c9d408d
Luca Bernstein <luca.bernstein@sap.com>
2026-05-12 09:19:12 +0200

    Fix kcp provider: pass original config, not virtual workspace URL
    The apiexport.New provider needs the kcp admin config to build a cache
    for APIExportEndpointSlice on the control plane. Previously it received
    the virtual workspace URL config which doesn't serve those APIs.

--- cmd/main.go
+++ cmd/main.go
@@ -216,23 +216,22 @@ func main() {
 	if isKcp, err = util.HasKcpAPIGroups(restConfig); err != nil {
 		setupLog.Error(err, "to determine if kcp API Group is present")
 	}
-	if isKcp {
-		setupLog.Info("Found KCP APIs, looking up virtual workspace URL")
-		restConfig, err = util.RestConfigForLogicalClusterHostingAPIExport(mgrContext, restConfig, apiExportName)
-		if err != nil {
-			setupLog.Error(err, "looking up virtual workspace URL")
-			os.Exit(1)
-		}
-	}
 
 	var provider util.ProviderWithRun
 	if isKcp {
+		setupLog.Info("Found KCP APIs, looking up virtual workspace URL")
 		provider, err = apiexport.New(restConfig, apiExportName, apiexport.Options{
 			Scheme:        controlplanev1alpha1.Scheme,
 			ObjectToWatch: &apisv1alpha2.APIBinding{},
 		})
 		if err != nil {
 			setupLog.Error(err, "unable to create kcp Provider")
+			os.Exit(1)
+		}
+
+		restConfig, err = util.RestConfigForLogicalClusterHostingAPIExport(mgrContext, restConfig, apiExportName)
+		if err != nil {
+			setupLog.Error(err, "looking up virtual workspace URL")
 			os.Exit(1)
 		}
 	}
