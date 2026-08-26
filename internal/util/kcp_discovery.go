af4ad8494eb9fc9994bad6ce07897a46e330ec7f
Marc Vornetran <marc.vornetran@sap.com>
2026-08-26 13:55:53 +0200

    Update copyright text
    Signed-off-by: Marc Vornetran <marc.vornetran@sap.com>

--- internal/util/kcp_discovery.go
+++ internal/util/kcp_discovery.go
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-Modifications Copyright SAP SE or an SAP affiliate company and Gardener contributors
+Modifications Copyright Copyright Contributors to the Gardener project
 */
 
 package util
